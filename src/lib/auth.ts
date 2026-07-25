441cce00233a3c7f780271ce0aa709e8fc598e51
RAMSANKARANAND <cramsankaranand@gmail.com>
2026-07-25 16:56:07 +0530

    Add trustHost to fix NextAuth UntrustedHost error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import { getDbAsync } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       name: "credentials",
